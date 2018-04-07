edf9d07aebf13c407c6ecdcf284563bab3d3617c
Yuming Wei <wymice@gmail.com>
2018-04-07 15:11:03 -0400

    Refactor login vue and controller.

--- src/main/frontend/src/components/auth/Login.vue
+++ src/main/frontend/src/components/auth/Login.vue
@@ -2,21 +2,21 @@
   <div class="login">
     <!-- <h2>{{'auth.welcome' | translate}}</h2> -->
     <h2>Welcome</h2>
-    <form method="post" action="/auth/login" name="login">
+    <form method="post" name="login">
       <div class="form-group">
         <div class="input-group">
-          <input type="text" id="email" required="required"/>
-          <label class="control-label" for="email">Email</label><i class="bar"></i>
+          <input type="text" id="username" v-model="loginForm.username" required="required"/>
+          <label class="control-label" for="username">Username</label><i class="bar"></i>
         </div>
       </div>
       <div class="form-group">
         <div class="input-group">
-          <input type="password" id="password" required="required"/>
+          <input type="password" id="password" v-model="loginForm.password" required="required"/>
           <label class="control-label" for="password">Passwrod</label><i class="bar"></i>
         </div>
       </div>
       <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between down-container">
-        <button class="btn btn-primary" type="submit">
+        <button class="btn btn-primary" type="submit" v-on:click.prevent="handleLogin">
           Login
         </button>
         <router-link class='link' :to="{name: 'Register'}">Register</router-link>
@@ -26,33 +26,53 @@
 </template>
 
 <script>
-  export default {
-    name: 'login'
+export default {
+  name: "login",
+  data() {
+    return {
+      loginForm: {
+        username: "dcmax20",
+        password: "dcmax"
+      },
+      loginSuccess: ""
+    };
+  },
+  methods: {
+    handleLogin() {
+      console.log(this.loginForm);
+      this.$http
+        .post("http://localhost:8080/users/login", this.loginForm)
+        .then(function(data) {
+          console.log(data);
+          this.loginSuccess = data.bodyText;
+        });
+    }
   }
+};
 </script>
 
 <style lang="scss">
-  @import '../../assets/sass/variables';
-  @import '~bootstrap/scss/mixins/breakpoints';
-  @import "~bootstrap/scss/functions";
-  @import '~bootstrap/scss/variables';
-  .login {
-    @include media-breakpoint-down(md) {
-      width: 100%;
-      padding-right: 2rem;
-      padding-left: 2rem;
-      .down-container {
-        .link {
-          margin-top: 2rem;
-        }
+@import "../../assets/sass/variables";
+@import "~bootstrap/scss/mixins/breakpoints";
+@import "~bootstrap/scss/functions";
+@import "~bootstrap/scss/variables";
+.login {
+  @include media-breakpoint-down(md) {
+    width: 100%;
+    padding-right: 2rem;
+    padding-left: 2rem;
+    .down-container {
+      .link {
+        margin-top: 2rem;
       }
     }
-    h2 {
-      text-align: center;
-    }
-    width: 21.375rem;
-    .down-container {
-      margin-top: 3.125rem;
-    }
   }
+  h2 {
+    text-align: center;
+  }
+  width: 21.375rem;
+  .down-container {
+    margin-top: 3.125rem;
+  }
+}
 </style>
--- src/main/java/dcmax/controllers/LoginController.java
+++ src/main/java/dcmax/controllers/LoginController.java
@@ -1,20 +1,16 @@
 package dcmax.controllers;
 
 import dcmax.forms.LoginForm;
-import dcmax.models.User;
 import dcmax.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 import static dcmax.utils.constants.CommonConstants.FAIL_MSG;
 import static dcmax.utils.constants.CommonConstants.SUCCESS_MSG;
 
+@CrossOrigin
 @RestController
 public class LoginController {
 
@@ -23,7 +19,7 @@ public class LoginController {
 
     @PostMapping(value = "/users/login")
     public String userLogin(@RequestBody @Valid LoginForm loginForm) {
-        return userService.authenticate(loginForm) ? SUCCESS_MSG : FAIL_MSG;
+        return userService.authenticate (loginForm) ? SUCCESS_MSG : FAIL_MSG;
     }
 
 }
